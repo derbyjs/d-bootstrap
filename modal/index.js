911cbf28a7b12ce1177b913c7411ba2f5bdaa077
nateps <nate@nateps.com>
2012-05-30 09:14:20 -0700

    update modal to new component custom event api

--- modal/index.js
+++ modal/index.js
@@ -1,18 +1,20 @@
 exports.create = function(model, dom) {
-  function close(button) {
-    var cancelled = model.trigger('close', button)
+  var self = this;
+
+  this.close = function(action) {
+    var cancelled = this.emitCancellable('close', action)
     if (cancelled) return
     model.set('show', false)
   }
 
   dom.addListener(document, 'keydown', function(e) {
     if (e.keyCode === 27) {  // Escape
-      close('escape')
+      self.close('escape')
     }
   })
 
   this.click = function(e) {
-    var button = e.target.getAttribute('data-button')
-    if (button) close(button)
+    var action = e.target.getAttribute('data-action')
+    if (action) self.close(action)
   }
 }
