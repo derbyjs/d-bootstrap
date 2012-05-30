e2863791ef475264d71acbeef0f43a23054577d3
nateps <nate@nateps.com>
2012-05-30 09:14:50 -0700

    add support for a library styles option to specify css includes

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-var config = {
-  ns: 'boot'
-, filename: __filename
-, styles: __dirname + '/node_modules/bootstrap/less/bootstrap'
-, scripts: {
-    dropdown: require('./dropdown')
-  , modal: require('./modal')
-  , tabs: require('./tabs')
-  , tab: {}
-  }
-}
+var lessRoot = __dirname + '/node_modules/bootstrap/less/'
+  , config = {
+      ns: 'boot'
+    , filename: __filename
+    , scripts: {
+        dropdown: require('./dropdown')
+      , modal: require('./modal')
+      , tabs: require('./tabs')
+      , tab: {}
+      }
+    }
 
 module.exports = boot
 boot.decorate = 'derby'
 
 function boot(derby, options) {
-  derby.createLibrary(config, options)
+  var outConfig = Object.create(config)
+    , styles = options && options.styles
+    , outStyles, i, len, style
+
+  if (styles) {
+    outStyles = []
+    for (i = 0, len = styles.length; i < len; i++) {
+      outStyles.push(lessRoot + styles[i]) 
+    }
+  } else {
+    outStyles = lessRoot + 'bootstrap'
+  }
+  outConfig.styles = outStyles
+  derby.createLibrary(outConfig, options)
+  return this
 }
