95d611e167ad551b0338951a91468df2b71bdfb5
git-j <joerg+git-j@refeus.de>
2013-03-13 16:22:07 +0100

    trying to make pills work like tabs [not working]

--- nav/index.js
+++ nav/index.js
@@ -4,20 +4,25 @@ exports.init = function(model) {
     , type = this.type = model.get('type')
     , brand = this.brand = model.get('brand')
 
+  //console.log('nav:' + type + model.get('content'));
+
   this.on('init:child', function(child, type) {
+    console.log(type);
     if (type == 'lib:tab_pane') {
       var childModel = child.model
         , name = childModel.get('name') || nav.get('length') || 0
         , title = childModel.get('title')
         , header = childModel.get('header')
       nav.push({name: name, title: title, header: header, model: childModel})
+      //console.log({name: name, title: title, header: header});
       this.select()
     } else if (type == 'lib:link') {
       var childModel = child.model
         , name = childModel.get('name') || nav.get('length') || 0
         , disabled = childModel.get('disabled')
         , header = childModel.get('header')
-      nav.push({name: name, header: header, disabled: disabled, model: childModel})
+      nav.push({name: name, header: header, disabled: disabled, model: childModel});
+      console.log('link',{name: name, header: header, disabled: disabled});
     } else if (type == 'lib:page') {
       var childModel = child.model
         , name = childModel.get('name') || nav.get('length') || 0
