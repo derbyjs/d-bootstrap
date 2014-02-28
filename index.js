116db76d2f19fc00945dee25405a96be415d834a
Alexander Wenzowski <alexander@wenzowski.com>
2014-02-27 22:27:22 -0500

    loadStyles path should not contain `css` extension

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = function(app, options) {
   app.component(require('./dropdown'));
   app.component(require('./modal'));
   app.component(require('./tabs'));
-  app.loadStyles(__dirname + '/node_modules/bootstrap/dist/css/bootstrap.min.css');
+  app.loadStyles(__dirname + '/node_modules/bootstrap/dist/css/bootstrap.min');
 };
