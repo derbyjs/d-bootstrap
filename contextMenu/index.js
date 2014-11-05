007948f232a3c49e1f6cdb03e7dc078684c19dbb
Daði Snær Skúlason <dadisn@gmail.com>
2014-11-05 13:08:42 +0100

    Added contextMenu component
    — The context menu is opened with the open() function. This function
    can be called from a HTML element’s on-contextmenu (i.e. <element
    on-contextmenu=“contextmenu.open()” />).
    — Menu options for the context menu are defined as an array of objects.
    Each object can contain the following
      — content: The text to display in the menu and to save as a selected
    value
      — class: CSS class for the <li> element in the context menu dropdown
    (can be used to insert dividers, for example)
      — value: Value to be saved when selected, overwrites content as value
    if provided
      — action: Function to be run when the option is selected
    — Actions to be taken when selecting a menu option can be
    — A delayable event “open” is triggered before opening the context menu
    — The “select” event is triggered after a option has been selected and
    the option’s action function (if any) has been called

---
+++ contextMenu/index.js
@@ -0,0 +1,39 @@
+module.exports = ContextMenu;
+function ContextMenu() {}
+ContextMenu.prototype.view = __dirname;
+
+ContextMenu.prototype.create = function(model, dom) {
+	// Close when clicking outside of the context menu
+	var contextMenu = this;
+	dom.on('click', function(e) {
+		if(contextMenu.menu.contains(e.target)) return;
+		model.set('open', false);
+	});
+};
+
+ContextMenu.prototype.open = function(e) {
+	if(!e) throw new Error('You must provide a click event when opening the context menu.');
+
+	e.preventDefault();
+	var contextMenu = this;
+	var model = this.model;
+	var x = e.clientX + 'px';
+	var y = e.clientY + 'px';
+
+	this.emitDelayable('open', function() {
+		contextMenu.menu.style.top = y;
+		contextMenu.menu.style.left = x;
+		model.set('open', true);
+	});
+};
+
+ContextMenu.prototype.select = function(option) {
+  this.model.set('value', optionValue(option));
+  this.model.set('open', false);
+  if(option.hasOwnProperty('action') && typeof option.action === 'function') option.action();
+  this.emit('select');
+};
+
+function optionValue(option) {
+  return (option.hasOwnProperty('value')) ? option.value : option.content;
+}
--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = function(app, options) {
   app.component(require('./dropdown'));
   app.component(require('./modal'));
   app.component(require('./tabs'));
+  app.component(require('./contextMenu'));
   app.loadStyles(__dirname + '/css/bootstrap.min');
 };
