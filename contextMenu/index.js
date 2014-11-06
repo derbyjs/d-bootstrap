8565173790ca5295a504d21c2fcb5ac0692f2b02
Daði Snær Skúlason <dadisn@gmail.com>
2014-11-06 20:57:51 +0100

    Convert tabs to spaces

--- contextMenu/index.js
+++ contextMenu/index.js
@@ -3,28 +3,28 @@ function ContextMenu() {}
 ContextMenu.prototype.view = __dirname;
 
 ContextMenu.prototype.create = function(model, dom) {
-	// Close when clicking outside of the context menu
-	var contextMenu = this;
-	dom.on('click', function(e) {
-		if(contextMenu.menu.contains(e.target)) return;
-		model.set('open', false);
-	});
+  // Close when clicking outside of the context menu
+  var contextMenu = this;
+  dom.on('click', function(e) {
+    if(contextMenu.menu.contains(e.target)) return;
+    model.set('open', false);
+  });
 };
 
 ContextMenu.prototype.open = function(e) {
-	if(!e) throw new Error('You must provide a click event when opening the context menu.');
+  if(!e) throw new Error('You must provide a click event when opening the context menu.');
 
-	e.preventDefault();
-	var contextMenu = this;
-	var model = this.model;
-	var x = e.clientX + 'px';
-	var y = e.clientY + 'px';
+  e.preventDefault();
+  var contextMenu = this;
+  var model = this.model;
+  var x = e.clientX + 'px';
+  var y = e.clientY + 'px';
 
-	this.emitDelayable('open', function() {
-		contextMenu.menu.style.top = y;
-		contextMenu.menu.style.left = x;
-		model.set('open', true);
-	});
+  this.emitDelayable('open', function() {
+    contextMenu.menu.style.top = y;
+    contextMenu.menu.style.left = x;
+    model.set('open', true);
+  });
 };
 
 ContextMenu.prototype.select = function(option) {
