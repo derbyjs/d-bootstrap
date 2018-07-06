e8f40ee4bd537215370224d8f92b744133367683
Nate Smith <nate@nateps.com>
2018-07-06 12:59:57 -0700

    test for existence of require.resolve for use in browser

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function(app, options) {
   app.component(require('./tabs'));
   app.component(require('./alert'));
   app.component(require('./contextMenu'));
-  if (!options || (options && options.loadStyles)) {
+  if (require && require.resolve && !options || (options && options.loadStyles)) {
     var filename = require.resolve('bootstrap/dist/css/bootstrap.min.css');
     app.loadStyles(filename);
   }
